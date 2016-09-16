d74f2c5f1e4113fde8039414c482867b81d1eb86
Edward Kemp <edwardlanekemp@gmail.com>
2016-09-16 10:35:04 +0100

    Fixed heroku server issue

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 var dbURIs = {
   test: 'mongodb://localhost/api-test',
   development: 'mongodb://localhost/api-development',
-  production: process.env.MONGOLAB_URI || 'mongodb://localhost/api'
+  production: process.env.MONGODB_URI || 'mongodb://localhost/api'
 
 }
 
